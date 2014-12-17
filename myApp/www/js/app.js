41d49afeb57235eaf1ff7d5505303f62dacff56b
a3rosol <a3rosol@gmail.com>
2014-12-17 10:20:46 +0100

    1st Version with working Facebook Login procedure

---
+++ myApp/www/js/app.js
@@ -0,0 +1,116 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngMap', 'firebase'])
+
+.run(function($ionicPlatform, $rootScope, $state) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    /*  setTimeout(function() {
+    $cordovaSplashscreen.hide()
+  }, 5000)*/
+  });
+    $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireAuth promise is rejected
+    // and redirect the user back to the home page
+    if (error === "AUTH_REQUIRED") {
+      $state.go("home");
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('index', {
+      url: "/index",
+      templateUrl: "templates/splash.html",
+      controller: "loginCtrl"
+    })
+
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+    })
+
+    .state('app.splash', {
+      url: "/splash",
+      views: {
+        'menuContent' :{
+          controller: 'loginCtrl',
+          templateUrl: "templates/splash.html"
+        }
+      }
+    })
+
+    .state('app.home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html",
+          controller: "homeCtrl"
+        }
+      }
+    })
+
+    .state('app.results', {
+      url: "/results",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/results.html",
+          controller: "resultCtrl"
+        }
+      }
+    })
+     .state('app.test', {
+      url: "/test",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/test.html",
+          controller: "SampleCtrl"
+        }
+      }
+    })
+    .state('app.profile', {
+      url: "/profile",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/profile.html"
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+    .state('app.single', {
+      url: "/playlists/:playlistId",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/playlist.html",
+          controller: 'PlaylistCtrl'
+        }
+      }
+    });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/index');
+});
+
---
+++ myApp/www/js/controllers.js
@@ -0,0 +1,310 @@
+angular.module('starter.controllers', [])
+    .factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
+        var ref = new Firebase("https://huggr.firebaseio.com/");
+        return $firebaseAuth(ref);
+    }])
+
+.controller('loginCtrl', function($scope, $firebase, $ionicModal, Auth, $state) {
+
+var ref = new Firebase("https://huggr.firebaseio.com/");
+var sync = $firebase(ref).$asObject();
+
+console.log(sync);
+
+    $scope.auth = Auth;
+
+    //create child for signin
+$scope.dataRef = $firebase(ref.child("users").child("data")).$asArray();
+
+    //create child for google
+$scope.googleRef = $firebase(ref.child("users").child("signin").child("google")).$asArray();
+$scope.facebookRef = $firebase(ref.child("users").child("signin").child("facebook")).$asArray();
+$scope.facebookObj = $firebase(ref.child("users").child("signin").child("facebook"));
+
+
+
+    $scope.login = function(authProvider)  {
+        if (authProvider == "google") {
+            $scope.auth.$authWithOAuthPopup("google").then(function(authData) {
+                    var userSigninIdentifier = authData.google.id;
+                    googleRef.once("value", function(checkSnapshot) {
+                            if (checkSnapshot.hasChild(userSigninIdentifier)) {
+
+                                googleRef.child(userSigninIdentifier).once("value", function(snapshot) {
+                                    var profileID = snapshot.val().profileID;
+                                    //update data in db
+                                    googleRef.child(userSigninIdentifier).update({
+                                        token: authData.token,
+                                        expires: authData.expires,
+                                        AccessToken: authData.google.accessToken
+                                    });
+
+                                    dataRef.child(profileID).update({
+                                        displayName: authData.google.displayName,
+                                        email: authData.google.email,
+                                        picture: authData.google.cachedUserProfile.picture
+                                    });
+                                    console.log("Logged in as google:", authData.uid);
+                                    $state.go('app.home');
+                                });
+                            } else {
+                                register(authProvider, authData);
+                            }
+                        });
+                    }).catch(function(error) {console.error("Authentication failed:", error)}); //authGoogle
+        }
+        if (authProvider == "facebook") {
+
+            $scope.auth.$authWithOAuthPopup("facebook").then(function(authData) {
+
+                var userSigninIdentifier = authData.facebook.id;
+                console.log("userSigninIdentifier:" + userSigninIdentifier);
+
+                //
+
+                if ($scope.facebookRef.$getRecord(userSigninIdentifier) == null) {
+                	console.warn("new user, registering...");
+    					$scope.register(authProvider, authData);
+					} else {
+					 $scope.profileID = $scope.facebookRef.$getRecord(userSigninIdentifier).profileID;
+					 $firebase(ref.child("users").child("signin").child("facebook").child(userSigninIdentifier)).$update({
+                                token: authData.token,
+                                expires: authData.expires,
+                                AccessToken: authData.facebook.accessToken
+                            });
+                            $firebase(ref.child("users").child("data").child($scope.profileID)).$update({
+                                displayName: authData.facebook.displayName,
+                                email: authData.facebook.email,
+                                picture: authData.facebook.cachedUserProfile.picture.data.url
+                            });
+                            console.log("Logged in as:", authData.uid);
+                            $state.go('app.home');
+					}
+            }).catch(function(error) {
+                console.error("Authentication failed facebook:", error);
+            });
+
+        }
+    };
+
+$scope.register = function (authProvider, authData) {
+
+    var newProfileID = Math.floor(Math.random() * (9999999999 - 1000000000 + 1) + 1000000000);
+
+        while ($scope.dataRef.$getRecord(newProfileID) != null) {
+            newProfileID = Math.floor(Math.random() * (9999999999 - 1000000000 + 1) + 1000000000);
+        }
+
+        if (authProvider == "google") {
+            //write authentification data into database
+
+              $firebase(ref.child("users").child("signin").child("google")).$set(authData.google.id);
+               	$firebase(ref.child("users").child("signin").child("google").child(authData.google.id)).$set({
+      			displayName: authData.google.displayName,
+                token: authData.token,
+                expires: authData.expires,
+                uid: authData.uid,
+                ID: authData.google.id,
+                AccessToken: authData.google.accessToken,
+                profileID: newProfileID
+            });
+             $firebase(ref.child("users").child("data")).$set(newProfileID);
+             $firebase(ref.child("users").child("data").child(newProfileID)).$set({
+ 				profileID: newProfileID,
+                googleID: authData.google.id,
+                displayName: authData.google.displayName,
+                email: authData.google.email,
+                picture: authData.google.cachedUserProfile.picture,
+                gender: authData.google.cachedUserProfile.gender,
+                firstname: authData.google.cachedUserProfile.given_name,
+                lastname: authData.google.cachedUserProfile.family_name
+			});
+        }
+        if (authProvider == "facebook") {
+            //write authentification data into database
+  				$firebase(ref.child("users").child("signin").child("facebook")).$set(authData.facebook.id);
+               	$firebase(ref.child("users").child("signin").child("facebook").child(authData.facebook.id)).$set({
+                displayName: authData.facebook.displayName,
+                token: authData.token,
+                expires: authData.expires,
+                uid: authData.uid,
+                ID: authData.facebook.id,
+                AccessToken: authData.facebook.accessToken,
+                profileID: newProfileID
+            });
+             $firebase(ref.child("users").child("data")).$set(newProfileID);
+             $firebase(ref.child("users").child("data").child(newProfileID)).$set({
+             	profileID: newProfileID,
+                googleID: null,
+                facebookID: authData.facebook.id,
+                displayName: authData.facebook.displayName,
+                email: authData.facebook.email,
+                picture: authData.facebook.cachedUserProfile.picture.data.url,
+                gender: authData.facebook.cachedUserProfile.gender,
+                firstname: authData.facebook.cachedUserProfile.first_name,
+		        lastname: authData.facebook.cachedUserProfile.last_name
+			});
+        }
+       $state.go('app.home');
+
+
+}; //function register(authProvider)
+
+    //$scope.user = $scope.auth.$getAuth();
+
+    $ionicModal.fromTemplateUrl('templates/tos.html', {
+        scope: $scope
+    }).then(function(modalTos) {
+        $scope.modalTos = modalTos;
+    });
+
+    // Triggered in the login modalTos to close it
+    $scope.closeTos = function() {
+        $scope.modalTos.hide();
+    };
+
+    // Open the login modalTos
+    $scope.tos = function() {
+        $scope.modalTos.show();
+    };
+
+    $ionicModal.fromTemplateUrl('templates/privacy.html', {
+        scope: $scope
+    }).then(function(modalPriv) {
+        $scope.modalPriv = modalPriv;
+    });
+
+    // Triggered in the login modalPriv to close it
+    $scope.closePriv = function() {
+        $scope.modalPriv.hide();
+    };
+
+    // Open the login modalPriv
+    $scope.privacy = function() {
+        $scope.modalPriv.show();
+    };
+})
+
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+})
+
+
+
+.controller("SampleCtrl", ["$scope", "Auth",
+    function($scope, Auth) {
+        $scope.auth = Auth;
+        $scope.user = $scope.auth.$getAuth();
+        /* auth.$authWithOAuthPopup("facebook").then(function(authData) {
+           console.log("Logged in as:", authData.uid);
+         }).catch(function(error) {
+           console.error("Authentication failed: ", error);
+         });*/
+    }
+])
+
+.controller('PlaylistsCtrl', function($scope) {
+    $scope.playlists = [{
+        title: 'Reggae',
+        id: 1
+    }, {
+        title: 'Chill',
+        id: 2
+    }, {
+        title: 'Dubstep',
+        id: 3
+    }, {
+        title: 'Indie',
+        id: 4
+    }, {
+        title: 'Rap',
+        id: 5
+    }, {
+        title: 'Cowbell',
+        id: 6
+    }];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {})
+
+.controller('homeCtrl', function($scope, $ionicLoading, $cordovaGeolocation, $ionicPopover, $state) {
+
+
+        $scope.positions = [{
+            lat: 43.07493,
+            lng: -89.381388
+        }];
+
+        $scope.$on('mapInitialized', function(event, map) {
+            $scope.map = map;
+            $cordovaGeolocation
+                .getCurrentPosition()
+                .then(function(position) {
+                    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    $scope.positions.lat = pos.k;
+                    $scope.positions.lng = pos.B;
+                    $scope.map.setCenter(pos);
+
+                }, function(err) {
+                    alert("error locating the user");
+                });
+        });
+
+        $ionicPopover.fromTemplateUrl('templates/popovers/hugSettings.html', {
+            scope: $scope,
+        }).then(function(popover) {
+            $scope.popover = popover;
+        });
+        $scope.openPopover = function($event) {
+            $scope.popover.show($event);
+        };
+        $scope.closePopover = function() {
+            $scope.popover.hide();
+        };
+        //Cleanup the popover when we're done with it!
+        $scope.$on('$destroy', function() {
+            $scope.popover.remove();
+        });
+        // Execute action on hide popover
+        $scope.$on('popover.hidden', function() {
+            // Execute action
+        });
+        // Execute action on remove popover
+        $scope.$on('popover.removed', function() {
+            // Execute action
+        });
+
+        $scope.displayResults = function() {
+            $state.go('app.results');
+        }
+
+    })
+    .controller('resultCtrl', function($scope) {
+        $scope.results = [];
+        for (var i = 0; i < 5; i++) {
+            $scope.results[i] = {
+                name: i + 1,
+                items: []
+            };
+            for (var j = 0; j < 3; j++) {
+                $scope.results[i].items.push(i + '-' + j);
+            }
+        }
+        console.log($scope.groups);
+
+        /*
+         * if given group is the selected group, deselect it
+         * else, select the given group
+         */
+        $scope.toggleGroup = function(group) {
+            if ($scope.isGroupShown(group)) {
+                $scope.shownGroup = null;
+            } else {
+                $scope.shownGroup = group;
+            }
+        };
+        $scope.isGroupShown = function(group) {
+            return $scope.shownGroup === group;
+        };
+    });
