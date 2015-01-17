20f746617ebca04f190e6393f85f613825eb4c24
a3rosol <a3rosol@gmail.com>
2015-01-17 13:40:14 +0100

    Introduced some minor Changes UI and Ionic Update
    Ionic Update
    Star Rating in Profile
    #10 now fixed

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngMap', 'firebase'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngMap', 'firebase', 'ionic.rating'])
 
 .run(function($ionicPlatform, $rootScope, $state, $firebase, $firebaseAuth, Auth, UserInfo) {
     $ionicPlatform.ready(function() {
