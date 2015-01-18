688a43879c5ece6e17b53530a665da76b80aa2a6
Christian <klein.chris93@gmail.com>
2015-01-18 23:12:12 +0100

    fix 2 for notificcations #17

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -981,7 +981,7 @@ angular.module('starter.controllers', [])
                     picture: $scope.currentUser.picture,
                     time: today,
                     profileID: $scope.currentUser.profileID,
-                    type: "decline",
+                    type: "accept",
                     change: "remove"
                 }).then(function(x) {
                     console.log("successfully declined hugg!");
