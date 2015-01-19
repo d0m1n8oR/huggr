e6119d900ced551d20dff18116c1eb6cb45288a5
Christian <klein.chris93@gmail.com>
2015-01-19 11:21:45 +0100

    working on #48

---
+++ myApp/www/js/controllers/aboutCtrl.js
@@ -0,0 +1,5 @@
+.controller('aboutCtrl', function($scope, $cordovaGeolocation, $ionicPopover, $state, localstorage, $firebase, toast, notifications) {
+    //Setze Koordinaten für Initialisierung von Maps
+
+
+})
