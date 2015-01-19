33c405feb42fbb9edb069ef0d69f67b131ae66d7
a3rosol <a3rosol@gmail.com>
2015-01-19 21:49:07 +0100

    now shows new notifications

--- myApp/www/js/factories/notifications.js
+++ myApp/www/js/factories/notifications.js
@@ -1,33 +1,36 @@
-.filter('keylength', function(){
-  return function(input){
-    if(!angular.isObject(input)){
-      throw Error("Usage of non-objects with keylength filter!!")
-    }
-    return Object.keys(input).length;
-  }
-})
-.service('notifications', ["$rootScope", "$firebase", "$timeout", "toast",
-
-    function($rootScope, $firebase, $timeout, toast) {
-
-        return {
-            sync: function(ID) {
-                var ref = new Firebase("https://huggr.firebaseio.com/users/data/" + ID + "/notifications");
-                var obj = $firebase(ref).$asObject();
-
-                obj.$bindTo($rootScope, "notificationData").then(function() {
-                    for (var key in $rootScope.notificationData) {
-                    	toast.pop("new notification");
-                        /*if ($rootScope.notificationData.hasOwnProperty(key)) {
-                            console.log($rootScope.notificationData[key]);
-                            $timeout(function() {
-                                toast.pop($rootScope.notificationData[key].huggID);
-                            }, 5500);
-
-                        }*/
-                    };
-                });
+.filter('keylength', function() {
+        return function(input) {
+            if (!angular.isObject(input)) {
+                throw Error("Usage of non-objects with keylength filter!!")
             }
-        };
-    }
-])
+            return Object.keys(input).length;
+        }
+    })
+    .service('notifications', ["$rootScope", "$firebase", "$timeout", "toast",
+
+        function($rootScope, $firebase, $timeout, toast) {
+
+            return {
+                sync: function(ID) {
+                    var ref = new Firebase("https://huggr.firebaseio.com/users/data/" + ID + "/notifications");
+                    var obj = $firebase(ref).$asObject();
+
+                    obj.$bindTo($rootScope, "notificationData").then(function() {
+                        obj.$watch(function() {
+                            toast.pop("new notification!");
+                        });
+
+                        for (var key in $rootScope.notificationData) {
+                            /*if ($rootScope.notificationData.hasOwnProperty(key)) {
+                                console.log($rootScope.notificationData[key]);
+                                $timeout(function() {
+                                    toast.pop($rootScope.notificationData[key].huggID);
+                                }, 5500);
+
+                            }*/
+                        };
+                    });
+                }
+            };
+        }
+    ])
