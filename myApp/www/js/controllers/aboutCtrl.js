ea7fe5bcaee3a24ddd1c44c00971c9f6795fd1f2
Christian <klein.chris93@gmail.com>
2015-01-19 12:15:05 +0100

    working on #38

--- myApp/www/js/controllers/aboutCtrl.js
+++ myApp/www/js/controllers/aboutCtrl.js
@@ -2,10 +2,51 @@
     $scope.currentUser = localstorage.getObject('userData');
     var ref = new Firebase("https://huggr.firebaseio.com/");
     $scope.supportRef = $firebase(ref.child("admin").child("support")).$asArray();
+    $scope.orderSupportRef = $firebase(ref.child("admin").child("support").orderByChild('profileID').equalTo($scope.currentUser.profileID)).$asArray();
 
     $scope.request = {
         message: null
     }
+
+    $scope.support = {
+        requests: [],
+        answers: []
+    }
+
+    $scope.orderSupportRef.$loaded().then(function(data) {
+
+        var i = 0;
+        //loop to get all the elements
+        while (data.$keyAt(i) != null) {
+            var record = data.$getRecord(data.$keyAt(i));
+            if (record.done == 0) {
+                $scope.support.requests.push({
+                    "displayName": record.displayName,
+                    "firstName": record.firstName,
+                    "email": record.email,
+                    "time": record.time,
+                    "reqest": record.request,
+                    "done": record.done,
+                    "supportID": record.supportID,
+                    "profileID": record.profileID,                    
+                });
+            }
+            else{
+                $scope.support.answers.push({
+                    displayName: record.displayName,
+                    firstName: record.firstname,
+                    email: record.email,
+                    time: record.tome,
+                    reqest: record.request,
+                    done: record.done,
+                    supportID: record.supportID,
+                    profileID: record.profileID, 
+                    answer: record.answer
+                });
+            }
+            i++;
+        }
+    })
 
     $scope.sendRequest = function sendRequest() {
         //console.log($scope.request.message);
@@ -27,11 +68,13 @@
 
                 $firebase(ref.child("admin").child("support").child(supportID)).$set({
                     displayName: $scope.currentUser.displayName,
-                    firsName: $scope.currentUser.firstname,
+                    firstName: $scope.currentUser.firstname,
                     email: $scope.currentUser.email,
                     time: today,
-                    reqest: request,
-                    done: 0
+                    request: request,
+                    done: 0,
+                    supportID: supportID,
+                    profileID: $scope.currentUser.profileID
                 }).then(function(y) {
                     toast.pop("Successfully sent request");
                 });
