0408f6e8b7f2f55fb4acb938a47b78fefb16456f
a3rosol <a3rosol@gmail.com>
2015-01-21 14:00:17 +0100

    positions?

--- huggr/www/js/controllers/resultCtrl.js
+++ huggr/www/js/controllers/resultCtrl.js
@@ -1,151 +1,152 @@
 .controller('resultCtrl', function($scope, Auth, $firebase, $stateParams, localstorage, $cordovaGeolocation, $q, $ionicLoading, $http, $state, toast, huggActions, $ionicModal) {
 
-            //initialize all the stuff
-            $scope.auth = Auth;
-            $scope.user = $scope.auth.$getAuth();
-            var ref = new Firebase("https://huggr.firebaseio.com/");
-            $scope.huggRef = $firebase(ref.child("hugg")).$asArray();
-            $scope.currentUser = localstorage.getObject('userData');
-            //displays all huggs that suit the request
-            //if huggs are not answered, they are also not done or accepted
-            $scope.orderHuggRef = $firebase(ref.child("hugg").orderByChild('answered').equalTo(0).limitToFirst(100)).$asArray();
+        //initialize all the stuff
+        $scope.auth = Auth;
+        $scope.user = $scope.auth.$getAuth();
+        var ref = new Firebase("https://huggr.firebaseio.com/");
+        $scope.huggRef = $firebase(ref.child("hugg")).$asArray();
+        $scope.currentUser = localstorage.getObject('userData');
+        //displays all huggs that suit the request
+        //if huggs are not answered, they are also not done or accepted
+        $scope.orderHuggRef = $firebase(ref.child("hugg").orderByChild('answered').equalTo(0).limitToFirst(100)).$asArray();
 
-            var gender;
-            var range;
+        var gender;
+        var range;
 
-            //checks what gender is filtered on
-            if ($stateParams.female == "none" && $stateParams.male == "true") {
-                gender = "male";
-            }
-            if ($stateParams.male == "none" && $stateParams.female == "true") {
-                gender = "female";
-            }
-            if (($stateParams.male == "none" && $stateParams.female == "none") || ($stateParams.male == "true" && $stateParams.female == "true")) {
-                gender = "both";
-            }
+        //checks what gender is filtered on
+        if ($stateParams.female == "none" && $stateParams.male == "true") {
+            gender = "male";
+        }
+        if ($stateParams.male == "none" && $stateParams.female == "true") {
+            gender = "female";
+        }
+        if (($stateParams.male == "none" && $stateParams.female == "none") || ($stateParams.male == "true" && $stateParams.female == "true")) {
+            gender = "both";
+        }
 
-            $ionicModal.fromTemplateUrl('templates/modals/googleMaps.html', {
-                scope: $scope
-            }).then(function(modal) {
-                $scope.gmapsModal = modal;
+        $ionicModal.fromTemplateUrl('templates/modals/googleMaps.html', {
+            scope: $scope
+        }).then(function(modal) {
+            $scope.gmapsModal = modal;
+        });
+
+        $scope.modalData = {};
+        $scope.$on('mapInitialized', function(event, map) {
+            $scope.map = map;
+        });
+
+        $scope.showPosition = function(lat, long) {
+            $scope.modalData.lat = lat;
+            $scope.modalData.long = long;
+            $scope.gmapsModal.show();
+            $cordovaGeolocation.getCurrentPosition().then(function(position) {
+                //wandle in google Maps format um
+                var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                $scope.modalData.userlat = pos.k;
+                $scope.modalData.userlong = pos.D;
+            }, function(err) {
+                toast.pop("There was an error while we tried to locate you.");
             });
 
-            $scope.modalData = {};
-            $scope.$on('mapInitialized', function(event, map) {
-                $scope.map = map;
-            });
+        }
 
-            $scope.showPosition = function(lat, long) {
-                $scope.modalData.lat = lat;
-                $scope.modalData.long = long;
-                $scope.gmapsModal.show();
-                $cordovaGeolocation.getCurrentPosition().then(function(position) {
-                    //wandle in google Maps format um
-                    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                    $scope.modalData.userlat = pos.k;
-                    $scope.modalData.userlong = pos.D;
-                }, function(err) {
-                    toast.pop("There was an error while we tried to locate you.");
-                });
+        range = $stateParams.range;
 
-            }
+        $scope.answerHugg = function(huggID, profileID) {
+            huggActions.answerHugg(huggID, $scope.currentUser, profileID);
+        }
 
-            range = $stateParams.range;
+        $scope.requestHugg = function(huggID, profileID) {
+            huggActions.requestHugg($scope.currentUser, gender);
+        }
 
-            $scope.answerHugg = function(huggID, profileID) {
-                huggActions.answerHugg(huggID, $scope.currentUser, profileID);
-            }
+        //initialize JSON
+        var huggArray = {
+            hugg: []
+        }
 
-            $scope.requestHugg = function(huggID, profileID) {
-                huggActions.requestHugg($scope.currentUser, gender);
-            }
+        //start values
+        var currentLat = 49.472726;
+        var currentLong = 8.449496;
 
-            //initialize JSON
-            var huggArray = {
-                hugg: []
-            }
+        //wait for ref to load before continuing
+        function getHuggs() {
+                var deferred = $q.defer();
 
-            //start values
-            var currentLat = 49.472726;
-            var currentLong = 8.449496;
+                $scope.orderHuggRef.$loaded().then(function(data) {
+                    var i = 0;
+                    //get GPS locaion
+                    $cordovaGeolocation
+                        .getCurrentPosition()
+                        .then(function(position) {
+                         var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                            //save coordinates to var
+                            currentLat = pos.k;
+                            curentLong = pos.D;
 
-            //wait for ref to load before continuing
-            function getHuggs() {
-                    var deferred = $q.defer();
+                            //parse all elements of returning array
+                            while (data.$keyAt(i) != null) {
 
-                    $scope.orderHuggRef.$loaded().then(function(data) {
-                            var i = 0;
-                            //get GPS locaion
-                            $cordovaGeolocation.getCurrentPosition()
-                                .then(function(position) {
+                                function load() {
+                                    var def = $q.defer();
+                                    def.resolve(data.$getRecord(data.$keyAt(i)));
+                                    return def.promise
+                                }; //end function
+                                //check whether filter gender of searching person and gender of requestor match
+                                //check whether gender of searching person and filter of requestor match
+                                //check whether current user's profile ID is among the blocked profile IDs
+                                load().then(function(record) {
+                                    if (((gender == "both") || (gender != "both" && record.reqGender == gender)) && ((record.FilterGender == "both") || (record.FilterGender != "both" && record.FilterGender == $scope.currentUser.gender)) && (record.reqProfileID != $scope.currentUser.profileID)) {
 
-                                    //save coordinates to var
-                                    currentLat = position.coords.latitude;
-                                    curentLong = position.coords.longitude;
+                                        $scope.huggRef = $firebase(ref.child("hugg").child(record.huggID).child("blocked")).$asArray();
+                                        $scope.huggRef.$loaded().then(function(data) {
 
-                                    //parse all elements of returning array
-                                    while (data.$keyAt(i) != null) {
+                                            //check whether user is blocked in results                            
+                                            if (data.$getRecord($scope.currentUser.profileID) == null) {
 
-                                        function load() {
-                                            var def = $q.defer();
-                                            def.resolve(data.$getRecord(data.$keyAt(i)));
-                                            return def.promise
-                                        }; //end function
-                                        //check whether filter gender of searching person and gender of requestor match
-                                        //check whether gender of searching person and filter of requestor match
-                                        //check whether current user's profile ID is among the blocked profile IDs
-                                        load().then(function(record) {
-                                                if (((gender == "both") || (gender != "both" && record.reqGender == gender)) && ((record.FilterGender == "both") || (record.FilterGender != "both" && record.FilterGender == $scope.currentUser.gender)) && (record.reqProfileID != $scope.currentUser.profileID)) {
+                                                //calc distance
+                                                var radius = 6371;
+                                                var diffLat = (currentLat - record.reqLat) * (Math.PI / 180);
+                                                var diffLon = (currentLong - record.reqLong) * (Math.PI / 180);
+                                                var a =
+                                                    Math.sin(diffLat / 2) * Math.sin(diffLat / 2) +
+                                                    Math.cos((record.reqLat) * (Math.PI / 180)) * Math.cos((currentLat) * (Math.PI / 180)) *
+                                                    Math.sin(diffLon / 2) * Math.sin(diffLon / 2);
+                                                var b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                                                var distance = radius * b;
 
-                                                    $scope.huggRef = $firebase(ref.child("hugg").child(record.huggID).child("blocked")).$asArray();
-                                                    $scope.huggRef.$loaded().then(function(data) {
-
-                                                        //check whether user is blocked in results                            
-                                                        if (data.$getRecord($scope.currentUser.profileID) == null) {
-                                                            var service = new google.maps.DistanceMatrixService();
-                                                            service.getDistanceMatrix({
-                                                                origins: [currentLat + "," + currentLong],
-                                                                destinations: [record.reqLat + "," + record.reqLong],
-                                                                travelMode: google.maps.TravelMode.DRIVING
-                                                            }, function(result) {
-
-                                                                var results = result.rows[0].elements[0].distance;
-                                                                var distance = results.value;
-                                                                distance = (distance/1000);
-                                                                console.log(results.text);
-                                                                //check for distance within range and save to JSON
-                                                                if (distance <= range) {
-                                                                    huggArray.hugg.push({
-                                                                        "huggID": record.huggID,
-                                                                        "firstName": record.reqFirstName,
-                                                                        "gender": record.reqGender,
-                                                                        "lat": record.reqLat,
-                                                                        "long": record.reqLong,
-                                                                        "reqTime": record.reqTime,
-                                                                        "picture": record.reqPicture,
-                                                                        "profileID": record.reqProfileID,
-                                                                        "distance": results.text,
-                                                                        "rating": record.reqRating
-                                                                    }); //end push
-                                                                } // end if
-                                                            });
-                                                        }
-
-                                                    }); //end if
+                                                //check for distance within range and save to JSON
+                                                if (distance <= range) {
+                                                    huggArray.hugg.push({
+                                                        "huggID": record.huggID,
+                                                        "firstName": record.reqFirstName,
+                                                        "gender": record.reqGender,
+                                                        "lat": record.reqLat,
+                                                        "long": record.reqLong,
+                                                        "reqTime": record.reqTime,
+                                                        "picture": record.reqPicture,
+                                                        "profileID": record.reqProfileID,
+                                                        "distance": distance,
+                                                        "rating": record.reqRating
+                                                    }); //end push
                                                 } // end if
-                                                });//end then
-                                                i++;
-                                            } //end while
-                                            deferred.resolve(huggArray);
-                                        }); //end GPS then
 
-                                    //This is the return value
-                                }); //end load huggRef
-                            return deferred.promise;
-                        } //end function
+                                            } //end if
+                                        }); //end then
+                                    } // end if
+                                }); //end then
+                                i++;
+                            } //end while
+                            deferred.resolve(huggArray);
+                        }); //end GPS then
 
-                        getHuggs().then(function(array) {
-                            $scope.resultList = array;
-                        });
+                    //This is the return value
+                }); //end load huggRef
+                return deferred.promise;
+            } //end function
 
-                    }) //end resultCTRL
+        getHuggs().then(function(array) {
+            $scope.resultList = array;
+        });
+
+    }) //end resultCTRL
