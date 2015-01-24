73049663c80d7d1dbc54e3c050a99accf36922b6
a3rosol <a3rosol@gmail.com>
2015-01-24 03:00:41 +0100

    chat reworked, fixed most of the bugs

--- huggr/www/js/controllers/ChatCtrl.js
+++ huggr/www/js/controllers/ChatCtrl.js
@@ -1,8 +1,48 @@
+.directive('input', function($timeout) {
+    return {
+        restrict: 'E',
+        scope: {
+            'returnClose': '=',
+            'onReturn': '&',
+            'onFocus': '&',
+            'onBlur': '&'
+        },
+        link: function(scope, element, attr) {
+            element.bind('focus', function(e) {
+                if (scope.onFocus) {
+                    $timeout(function() {
+                        scope.onFocus();
+                    });
+                }
+            });
+            element.bind('blur', function(e) {
+                if (scope.onBlur) {
+                    $timeout(function() {
+                        scope.onBlur();
+                    });
+                }
+            });
+            element.bind('keydown', function(e) {
+                if (e.which == 13) {
+                    if (scope.returnClose) element[0].blur();
+                    if (scope.onReturn) {
+                        $timeout(function() {
+                            scope.onReturn();
+                        });
+                    }
+                }
+            });
+        }
+    }
+})
+
 .controller('ChatCtrl', function($scope, $stateParams, $firebase, localstorage, toast, $ionicScrollDelegate) {
 
     chatID = $stateParams.chatID;
     $scope.ctitle = $stateParams.chatTitle;
-    console.log("ChatID:" +chatID);
+    console.log("ChatID:" + chatID);
+
+    var isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
 
     //Reference to Firebase
     $scope.currentUser = localstorage.getObject('userData');
@@ -11,6 +51,23 @@
 
     $scope.messageInput = "";
     $scope.chatData;
+
+    $scope.inputUp = function() {
+        if (isIOS) $scope.data.keyboardHeight = 216;
+        $timeout(function() {
+            $ionicScrollDelegate.scrollBottom(true);
+        }, 300);
+
+    };
+
+    $scope.inputDown = function() {
+        if (isIOS) $scope.data.keyboardHeight = 0;
+        $ionicScrollDelegate.resize();
+    };
+
+    $scope.closeKeyboard = function() {
+        // cordova.plugins.Keyboard.close();
+    };
 
     $scope.sendMessage = function() {
         $scope.chatList.$add({
@@ -30,7 +87,9 @@
 
     var obj = sync.$asObject();
     obj.$loaded().then(function() {
-        $ionicScrollDelegate.scrollBottom();
-        obj.$bindTo($scope, "chatData");
+        obj.$bindTo($scope, "chatData").then(function() {
+            $ionicScrollDelegate.scrollBottom();
+
+        });
     });
 })
