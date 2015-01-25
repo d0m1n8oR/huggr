b5329044d5d2b12684dd424fa9cada177318ea50
Christian <klein.chris93@gmail.com>
2015-01-25 23:30:01 +0100

    tries on heatmap

--- huggrAdmin/www/js/controllers/statisticsCtrl.js
+++ huggrAdmin/www/js/controllers/statisticsCtrl.js
@@ -205,7 +205,7 @@
             var ctx = document.getElementById("analysis").getContext("2d");
             var analysisChart = new Chart(ctx).Line(lineData);
             $scope.legend.analysis = analysisChart.generateLegend();
-            console.log($scope.legend.analysis );
+            console.log($scope.legend.analysis);
             document.getElementById('analysisLabel').innerHTML = $scope.legend.analysis;
         })
     }
@@ -253,7 +253,7 @@
             var ctx = document.getElementById("gender").getContext("2d");
             var genderChart = new Chart(ctx).Doughnut(dData);
             $scope.legend.gender = genderChart.generateLegend();
-            console.log($scope.legend.gender );
+            console.log($scope.legend.gender);
             document.getElementById('genderLabel').innerHTML = $scope.legend.gender;
         })
     }
@@ -315,7 +315,7 @@
             var ctx = document.getElementById("status").getContext("2d");
             var statusChart = new Chart(ctx).Doughnut(dData);
             $scope.legend.status = statusChart.generateLegend();
-            console.log($scope.legend.status );
+            console.log($scope.legend.status);
             document.getElementById('statusLabel').innerHTML = $scope.legend.status;
         })
     }
@@ -331,5 +331,75 @@
 
     $scope.initGraph();
 
+    var map, pointarray, heatmap;
+
+    var taxiData = [
+        new google.maps.LatLng(37.782551, -122.445368),
+        new google.maps.LatLng(37.782745, -122.444586),
+        new google.maps.LatLng(37.782842, -122.443688),
+        new google.maps.LatLng(37.782919, -122.442815),
+        new google.maps.LatLng(37.782992, -122.442112),
+        new google.maps.LatLng(37.783100, -122.441461),
+        new google.maps.LatLng(37.783206, -122.440829),
+        new google.maps.LatLng(37.783273, -122.440324),
+        new google.maps.LatLng(37.783316, -122.440023)
+    ];
+
+
+
+    function initialize() {
+        var mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(37.774546, -122.433523),
+            mapTypeId: google.maps.MapTypeId.SATELLITE
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+        var pointArray = new google.maps.MVCArray(taxiData);
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: pointArray
+        });
+
+        heatmap.setMap(map);
+    }
+
+    function toggleHeatmap() {
+        heatmap.setMap(heatmap.getMap() ? null : map);
+    }
+
+    function changeGradient() {
+        var gradient = [
+            'rgba(0, 255, 255, 0)',
+            'rgba(0, 255, 255, 1)',
+            'rgba(0, 191, 255, 1)',
+            'rgba(0, 127, 255, 1)',
+            'rgba(0, 63, 255, 1)',
+            'rgba(0, 0, 255, 1)',
+            'rgba(0, 0, 223, 1)',
+            'rgba(0, 0, 191, 1)',
+            'rgba(0, 0, 159, 1)',
+            'rgba(0, 0, 127, 1)',
+            'rgba(63, 0, 91, 1)',
+            'rgba(127, 0, 63, 1)',
+            'rgba(191, 0, 31, 1)',
+            'rgba(255, 0, 0, 1)'
+        ]
+        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+    }
+
+    function changeRadius() {
+        heatmap.set('radius', heatmap.get('radius') ? null : 20);
+    }
+
+    function changeOpacity() {
+        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    initialize();
 
 })
