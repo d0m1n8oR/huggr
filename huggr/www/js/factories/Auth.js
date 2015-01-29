79250944995ae0c8df27269d3657e602d28f3155
Christian <klein.chris93@gmail.com>
2015-01-29 21:28:52 +0100

    working on referral to splash when logged out

--- huggr/www/js/factories/Auth.js
+++ huggr/www/js/factories/Auth.js
@@ -1,6 +1,12 @@
-.factory("Auth", ["$firebaseAuth",
-    function($firebaseAuth) {
+.factory("Auth", ["$firebaseAuth", "$state",
+    function($firebaseAuth, $state) {
         var ref = new Firebase("https://huggr.firebaseio.com/");
+        console.log($firebaseAuth(ref).$getAuth());
+        if($firebaseAuth(ref).$getAuth() == null)
+        {
+            console.log("hallo");
+            $state.go("index");
+        }
         return $firebaseAuth(ref);
     }
 ])
